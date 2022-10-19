774607cc57729226a79f370a027e3ac668e1b309
leann-labra <leann.labra@gmail.com>
2022-10-19 16:44:16 -0700

    restructured js after writing out pseudo code, figured out how to use moment to create my list

--- dynamic.js
+++ dynamic.js
@@ -2,47 +2,50 @@
 var currentDay = moment();
 $('#currentDay').text(currentDay.format('LLLL'));
 
+// var present = moment();
+
 //hours not defined? maybe jquery is not linked
-var hoursListEl = $('#hours');
-var hours = [
-    '9am',
-    '10am',
-    '11am',
-    '12pm',
-    '1pm',
-    '2pm',
-    '3pm',
-    '4pm',
-    '5pm',
-]
+var eventListEl = document.querySelector(".eventList");
+var hoursListEl = document.querySelector("#hours");
+
 //event list variables
 var eventListEl = $('eventList');
 
 //function to render the hours list
 function renderHours() {
+    var hours = [
+        '9am',
+        '10am',
+        '11am',
+        '12pm',
+        '1pm',
+        '2pm',
+        '3pm',
+        '4pm',
+        '5pm',
+    ]
     //moment time frame here? 
     for (var i = 0; i < hours.length; i++) {
-        //creatint hours list element
-        var hoursList = $('<li>');
-        //adding class to style from bootcamp to newly created list El
+        //create list of hours
+        var hoursList = $("<li>");
+        //assing style to list
         hoursList.addClass('list-group list-group-flush');
-        // //setting data attributes to hours
-        // hoursList.attr("text", hours[i]);
-        //rendering text for each list
+        // // //setting data attributes to hours
+        // hoursList.attr('data-time', hours[i]);
+        // //rendering text for each list
         hoursList.text(hours[i]);
-        //appending list to hours list element on html
+        // //appending list to hours list element on html
         hoursListEl.append(hoursList);
-
-        // hoursListEl.show();
-    };
-    console.log(hoursList);
-    console.log(hoursListEl);
+        // hours.ListEl.show(); 
+        // // hoursListEl.show();
+    }
 };
-       
+
+renderHours();
 
 function eventInputs() {
     //is input group defined? 
-    hoursListEl.append(input-group);
+    hoursListEl.append(eventListEl);
 
     //include moment here that will change time 
 
